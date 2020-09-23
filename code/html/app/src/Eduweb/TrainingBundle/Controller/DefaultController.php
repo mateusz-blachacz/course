228dbd6f25c70f49f826655ab631bef26499145c
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-09-23 15:03:13 +0200

    Annotations Cotroler https://trello.com/c/dnl9Sk1X

--- code/html/app/src/Eduweb/TrainingBundle/Controller/DefaultController.php
+++ code/html/app/src/Eduweb/TrainingBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Eduweb\TrainingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -24,6 +26,23 @@ class DefaultController extends Controller
     }
     public function simple2Action(){
         $responseMsq ='Simple 2';
+        return new \Symfony\Component\HttpFoundation\Response($responseMsq);
+
+    }
+
+    /**
+     * @Route(
+     *     "/register-tester/{name}-{age}-{role}",
+     *     name="eduweb_training_registerTester",
+     *     defaults={"role"="units"},
+     *     requirements={"age"="\d+","role"="units|functional"}
+     * )
+     *
+     * @Method({"GET"})
+     */
+    public function registerTesterAction($name, $age, $role){
+        $responseMsq = sprintf("Rejestracja testera o nazwie %s (wiek: %d) rola testera %s",$name,$age,$role);
+
         return new \Symfony\Component\HttpFoundation\Response($responseMsq);
 
     }

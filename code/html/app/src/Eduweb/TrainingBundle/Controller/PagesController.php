d046151f8b84cb9ba6f8f7e3880220dd4eed7652
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-09-24 13:16:23 +0200

    Learning forwording and redirect in Controllers https://trello.com/c/Refy6Dtg

--- code/html/app/src/Eduweb/TrainingBundle/Controller/PagesController.php
+++ code/html/app/src/Eduweb/TrainingBundle/Controller/PagesController.php
@@ -6,12 +6,38 @@ namespace Eduweb\TrainingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Validator\Tests\Fixtures\ToString;
 
 class PagesController extends Controller {
     public function aboutAction(){
         return new Response('about');
     }
+
+    public function aboutJsonAction(){
+        $json = array(
+            'name'=>'Buty',
+            'size'=> '32',
+            'price'=> '123.23'
+        );
+        return new Response(json_encode($json),Response::HTTP_OK,array('Content-type' => 'application/json'));
+
+
+    }
+
+    /**
+     * @Route ("/aboutTwig")
+     *
+     * @Template
+     */
+    public function aboutTwig(){
+       #  $this-> render('EduwebTrainingBundle:Pages:aboutTwig.html.twig');
+       # return  $this-> render('EduwebTrainingBundle:Pages:aboutTwig.html.twig');# parametry za pomoca array()
+        return array();
+    }
+
+
     /**
      * @Route ("/about_2")
     */
@@ -19,4 +45,42 @@ class PagesController extends Controller {
         return new Response('about');
     }
 
+    /**
+     * @Route(
+     *     "/go-to-page/{page_url}",
+     *     name="eduweb_training_goToPage",
+     *     defaults={"page_url"="aboutTwig"})
+     *
+     *
+     */
+    public function goToPageAction($page_url){
+        #return $this->redirect($this->generateUrl($page));
+        #return  $this->redirect("/app_dev.php/".$page_url);
+        $redirectUrl = $this->generateUrl('eduweb_training_simple2');
+
+        return $this->redirect($redirectUrl);
+
+    }
+
+    /**
+     * @Route("/print-header/{title}/{color}")
+     *
+     * @Template
+     */
+    public function printHeaderAction($title,$color){
+        return array(
+            'title'=>$title,
+            'color'=>$color
+        );
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contantPageAction(){
+        return $this->forward('EduwebTrainingBundle:Pages:printHeader',array(
+            'title'=>'Kontakt',
+            'color'=>'blue'
+        ));
+    }
 }

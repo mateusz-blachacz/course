1f27661edb8834f2708a6f09aff1d0127af1a940
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-09-24 09:54:51 +0200

    Add new routing https://trello.com/c/dnl9Sk1X

--- code/html/app/src/Eduweb/TrainingBundle/Controller/DefaultController.php
+++ code/html/app/src/Eduweb/TrainingBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace Eduweb\TrainingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation as shortRes;
+use Twig\Error\RuntimeError;
 
 class DefaultController extends Controller
 {
@@ -45,5 +48,56 @@ class DefaultController extends Controller
 
         return new \Symfony\Component\HttpFoundation\Response($responseMsq);
 
+    }
+
+    /**
+     * @Route("/generate-url")
+     */
+    public function generateUrlAction(){
+        #relative url
+        $response = $this->generateUrl('eduweb_training_registerUser',
+            array('name'=>'Jan',
+            'age'=>12));
+
+        return new \Symfony\Component\HttpFoundation\Response($response);
+    }
+
+    /**
+     * @Route("/generate-url-absolute")
+     */
+    public function generateUrl2Action(){
+        #absolute url
+        $response = $this->generateUrl('eduweb_training_registerUser',
+            array('name'=>'Kantana',
+                'age'=>56),TRUE);
+
+        return new \Symfony\Component\HttpFoundation\Response($response);
+    }
+
+    /**
+     * @Route(
+     *     "/generate/{method}",
+     *     defaults = {"method"="relative"},
+     *     requirements={"method"="absolute|relative"}
+     *     )
+     *
+     */
+    public function generateUrlExperiment2($method){
+        $response ="";
+        if("relative" == $method){
+            $response = $this->generateUrl('eduweb_training_simple2');
+
+        }elseif ("absolute" == $method){
+            $response = $this->generateUrl('eduweb_training_simple2',array(),TRUE);
+        }
+        return new shortRes\Response($response);
+    }
+
+    /**
+     * @Route("/gen")
+     */
+    public function generateUrlExperiment(){
+        $response = "Hello Experiment";
+        return  new shortRes\Response($response);
     }
 }

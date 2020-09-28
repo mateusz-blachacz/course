f528d36d50860d25eac9ecfb565fb69a80b77e7e
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-09-28 14:27:54 +0200

    Check error 500 https://trello.com/c/egrS2ZJ5

--- code/src/Eduweb/TrainingBundle/Controller/BlogController.php
+++ code/src/Eduweb/TrainingBundle/Controller/BlogController.php
@@ -24,6 +24,7 @@ class BlogController extends Controller
      *@Template
      */
     public function indexAction(){
+        throw new \Exception('nowy błąd');
         return array();
     }
     /**

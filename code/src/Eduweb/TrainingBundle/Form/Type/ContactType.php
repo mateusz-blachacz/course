16d478cbe9627e2630e121f9d106f30095ed246c
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-10-06 08:19:11 +0200

    Chanage form field type https://trello.com/c/R8x5ZmgU

--- code/src/Eduweb/TrainingBundle/Form/Type/ContactType.php
+++ code/src/Eduweb/TrainingBundle/Form/Type/ContactType.php
@@ -2,7 +2,12 @@
 
 namespace Eduweb\TrainingBundle\Form\Type;
 
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -17,10 +22,10 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("name", "text", array("label" => "Imię"))
-            ->add("email", "email", array("label" => "email"))
-            ->add("message", "textarea", array("label" => "Wiadomość"))
-            ->add("save", "submit", array("label" => "Zapisz"));
+            ->add("name", TextType::class, array("label" => "Imię"))
+            ->add("email", EmailType::class, array("label" => "email"))
+            ->add("message", TextareaType::class, array("label" => "Wiadomość"))
+            ->add("save", SubmitType::class, array("label" => "Zapisz"));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)

5f3d6ba3bf749e83df933a4d9b86f15a5c5375a1
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-10-06 08:29:34 +0200

    Replace deprecated function https://trello.com/c/R8x5ZmgU

--- code/src/Eduweb/TrainingBundle/Form/Type/ContactType.php
+++ code/src/Eduweb/TrainingBundle/Form/Type/ContactType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ContactType extends AbstractType
@@ -28,7 +29,7 @@ class ContactType extends AbstractType
             ->add("save", SubmitType::class, array("label" => "Zapisz"));
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array('data_class' => 'Eduweb\TrainingBundle\Entity\Contact'));
     }

3e03bad40e5aa8531f0e5e36c4b5c51ac53afc51
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-10-07 09:47:12 +0200

    remove old bundles from kernel https://trello.com/c/GFvGo1RW

--- code/app/AppKernel.php
+++ code/app/AppKernel.php
@@ -13,15 +13,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Eduweb\TrainingBundle\EduwebTrainingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;

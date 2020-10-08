a934a9d076f4d9cd4aa2c23ce540de725dd1a1d0
Mateusz Błachacz <mateuszblachacz@kramsoft.pl>
2020-10-08 08:04:53 +0200

    Use one if clause instead two https://trello.com/c/0kigiXF8

--- code/src/Eduweb/TrainingBundle/Controller/AdminController.php
+++ code/src/Eduweb/TrainingBundle/Controller/AdminController.php
@@ -73,7 +73,7 @@ class AdminController extends Controller
             $session = $this->get('session');
             $form->handleRequest($request);
 
-            if ($form->isValid()) {
+            if ($form->isSubmitted() && $form->isValid()) {
 
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($register);
